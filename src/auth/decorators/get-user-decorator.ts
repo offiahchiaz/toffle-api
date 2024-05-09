8e0fd503272e08fb3ad28f1b0f799ab114f7ea97
offiahchiaz <offiahchiaz@gmail.com>
2024-05-09 22:15:15 +0100

    Added auth decorators

---
+++ src/auth/decorators/get-user-decorator.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+
+import { ExecutionContext, createParamDecorator } from "@nestjs/common";
+
+
+export const GetUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request: Express.Request = ctx.switchToHttp().getRequest();
+    return request.user;
+  }
+)
