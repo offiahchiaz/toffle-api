7562312e9807eade9724f16bbdd60e4c4e018823
offiahchiaz <offiahchiaz@gmail.com>
2024-05-09 22:21:28 +0100

    Added user module and service

---
+++ src/user/user.controller.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorators';
+import { JwtGuard } from 'src/auth/guard';
+
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+  @Get('me')
+  getMe(@GetUser('') user: User) {
+    return  user;
+  }
+}
