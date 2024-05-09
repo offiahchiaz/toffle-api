15a9082111df1dddda8de9b78576fe1115be47f0
offiahchiaz <offiahchiaz@gmail.com>
2024-05-09 23:56:20 +0100

    Updated schema and test scripts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { TodoModule } from './todo/todo.module';
 
 
 @Module({
@@ -14,7 +14,7 @@ import { PrismaModule } from './prisma/prisma.module';
     }),
     AuthModule, 
     UserModule, 
-    BookmarkModule, 
+    TodoModule, 
     PrismaModule
   ],
 })
